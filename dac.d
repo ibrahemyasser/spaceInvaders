1b8bf389b8aceb5b9828c2807b7b59fd37c05b0e
Kareem Taha <82422224+KareemTahaAbdelfattah@users.noreply.github.com>
2023-05-19 18:45:11 +0300

    Add files via upload

---
+++ dac.d
@@ -0,0 +1,3 @@
+.\dac.o: DAC.c
+.\dac.o: DAC.h
+.\dac.o: .\APP\..//tm4c123gh6pm.h
---
+++ sound.d
@@ -0,0 +1,5 @@
+.\sound.o: Sound.c
+.\sound.o: .\APP\../DAC.h
+.\sound.o: Sound.h
+.\sound.o: .\APP\../SoundWaves.h
+.\sound.o: .\APP\..//tm4c123gh6pm.h
